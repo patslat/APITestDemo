e751f0d5b2e7e0132997a9c980257013172b21b0
Patrick Slattery <slattery.patrickj@gmail.com>
2013-10-01 18:38:02 -0700

    add CRUD to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+  respond_to :json
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    respond_with @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy # how do I handle destroys while using this pattern?
+      respond_with true
+    else
+      respond_with @post
+    end
+  end
+
+  def index
+    @posts = Post.all
+    respond_with @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    respond_with @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes(params[:post])
+    respond_with @post
+  end
+
 end
